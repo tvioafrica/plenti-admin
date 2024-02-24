f60580da15b5b0a0232773792c53f2ef141789ec
Temiye Oluseun <temiye92@gmail.com>
2024-02-24 20:32:09 +0100

    added functionality for mobile login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -88,6 +88,58 @@ class LoginController extends Controller
         ], 201);
     }
 
+    public function loginMobile(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'phone'    => ['required', 'string', 'max:255'],
+            'password' => ['required', 'string', 'min:6'],
+        ]);
+
+        if ($validator->fails()) {
+            return new JsonResponse([
+                'errors' => $validator->errors()
+            ], 422);
+        }
+        $request->merge(['status' => Status::ACTIVE]);
+
+        $user = User::where('phone', $request['phone'])->first();
+
+        if (!Auth::guard('web')->attempt(
+                ['email' => $user->email,'password' => $request['password'],'status' => $request['status']
+            ])) {
+            return new JsonResponse([
+                'errors' => ['validation' => trans('all.message.credentials_invalid')]
+            ], 400);
+        }
+
+        $branchId = Auth::user()->branch_id;
+        if (Auth::user()->branch_id == 0) {
+            $branchId = Settings::group('site')->get('site_default_branch');
+        }
+        $this->defaultAccessService->storeOrUpdate(['branch_id' => $branchId]);
+        $user        = $user;
+        $this->token = $user->createToken('auth_token')->plainTextToken;
+
+        if (!isset($user->roles[0])) {
+            return new JsonResponse([
+                'errors' => ['validation' => trans('all.message.role_exist')]
+            ], 400);
+        }
+
+        $permission        = PermissionResource::collection($this->permissionService->permission($user->roles[0]));
+        $defaultPermission = AppLibrary::defaultPermission($permission);
+
+        return new JsonResponse([
+            'message'           => trans('all.message.login_success'),
+            'token'             => $this->token,
+            'branch_id'         => (int)$user->branch_id,
+            'user'              => new UserResource($user),
+            'menu'              => MenuResource::collection(collect($this->menuService->menu($user->roles[0]))),
+            'permission'        => $permission,
+            'defaultPermission' => $defaultPermission,
+        ], 201);
+    }
+
     public function logout(Request $request): JsonResponse
     {
         $request->user()->currentAccessToken()->delete();
--- routes/api.php
+++ routes/api.php
@@ -115,6 +115,8 @@ Route::match(['get', 'post'], '/refresh-token', [RefreshTokenController::class, 
 
 Route::prefix('auth')->middleware(['installed', 'apiKey', 'localization'])->name('auth.')->namespace('Auth')->group(function () {
     Route::post('/login', [LoginController::class, 'login']);
+    Route::post('/login-mobile', [LoginController::class, 'loginMobile']);
+
 
     Route::prefix('forgot-password')->name('forgot-password.')->group(function () {
         Route::post('/', [ForgotPasswordController::class, 'forgotPassword']);
